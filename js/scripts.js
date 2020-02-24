04fda5d5de897d09794cc818d2e637230191c907
gracewahlman <59583145+gracewahlman@users.noreply.github.com>
2020-02-24 09:55:51 -0800

    CSS, JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log('OK');
+gsap.from('h1', {x: 1000, duration: 1});
+
+gsap.from('img', {x: -500, opacity: 0, duration: 3});
+
+gsap.to('img', {x: 0, duration: 3});
+
+gsap.from('#svg', {x: 1000, y: 500, scale: .5, duration: 1 })
