80e8ab7ad1130d84068e4c3d491d7bc5d057fa99
gracewahlman <59583145+gracewahlman@users.noreply.github.com>
2020-03-13 20:49:35 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-gsap.from('h1', {x: 2000, duration: 1, delay: 2.5});
+gsap.from('h1', {x: 2000, duration: .75, delay: 2.5});
 
-gsap.from('img', {x: -500, opacity: 0, duration: 3});
+gsap.from('img', {x: -500, opacity: 0, duration: 3.5});
 
-gsap.to('img', {x: 0, duration: 3});
+gsap.to('img', {x: 0, duration: 3.5});
 
 gsap.from('#svg', {x: 1000, y: 500, scale: .5, duration: 1 })
